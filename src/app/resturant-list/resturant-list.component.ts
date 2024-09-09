055cf4ed496d8f5cd0f16c9085e3680a864f4328
VishalShastra <codeshastraofficial@gmail.com>
2024-09-09 13:31:53 +0530

    angular app

--- src/app/resturant-list/resturant-list.component.ts
+++ src/app/resturant-list/resturant-list.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
 
+export interface Restaurant {
+  name: string;
+  imageUrl: string;
+  rating: number;
+  deliveryTime: string;
+}
+
 @Component({
   selector: 'app-resturant-list',
   templateUrl: './resturant-list.component.html',
   styleUrls: ['./resturant-list.component.css']
 })
 export class ResturantListComponent {
+
+  restaurants: Restaurant[] = [
+    {
+      name: 'Pizza Palace',
+      imageUrl: 'assets/pizza.jpeg',
+      rating: 4.5,
+      deliveryTime: '30-40 min'
+    },
+    {
+      name: 'Burger House',
+      imageUrl: 'assets/burger.jpeg',
+      rating: 4.2,
+      deliveryTime: '25-35 min'
+    },
+    {
+      name: 'Sushi Express',
+      imageUrl: 'assets/sushi.jpeg',
+      rating: 4.8,
+      deliveryTime: '45-55 min'
+    }
+  ];
 
 }
